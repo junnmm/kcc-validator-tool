f4b96877f6a7ff3074c20d021d8ea5689ab86967
junnmm <95088118+junnmm@users.noreply.github.com>
2022-07-25 23:00:10 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,261 @@
+import { Button, Col, Menu, Row } from "antd";
+import "antd/dist/antd.css";
+import {
+  useBalance,
+  useContractLoader,
+  useContractReader,
+  useGasPrice,
+  useOnBlock,
+  useUserProviderAndSigner,
+} from "eth-hooks";
+import { useExchangeEthPrice } from "eth-hooks/dapps/dex";
+import React, { useCallback, useEffect, useState } from "react";
+import { Link, Route, Switch, useLocation } from "react-router-dom";
+import "./App.css";
+import {
+  Account,
+  Contract,
+  Faucet,
+  GasGauge,
+  Header,
+  Ramp,
+  ThemeSwitch,
+  NetworkDisplay,
+  FaucetHint,
+  NetworkSwitch,
+} from "./components";
+import { NETWORKS, ALCHEMY_KEY } from "./constants";
+import externalContracts from "./contracts/external_contracts";
+// contracts
+import deployedContracts from "./contracts/hardhat_contracts.json";
+import { Transactor, Web3ModalSetup } from "./helpers";
+import { Home, ExampleUI, Hints, Subgraph } from "./views";
+import { useStaticJsonRPC } from "./hooks";
+
+const { ethers } = require("ethers");
+/*
+    Welcome to 🏗 scaffold-eth !
+
+    Code:
+    https://github.com/scaffold-eth/scaffold-eth
+
+    Support:
+    https://t.me/joinchat/KByvmRe5wkR-8F_zz6AjpA
+    or DM @austingriffith on twitter or telegram
+
+    You should get your own Alchemy.com & Infura.io ID and put it in `constants.js`
+    (this is your connection to the main Ethereum network for ENS etc.)
+
+
+    🌏 EXTERNAL CONTRACTS:
+    You can also bring in contract artifacts in `constants.js`
+    (and then use the `useExternalContractLoader()` hook!)
+*/
+
+/// 📡 What chain are your contracts deployed to?
+const initialNetwork = NETWORKS.localhost; // <------- select your target frontend network (localhost, rinkeby, xdai, mainnet)
+
+// 😬 Sorry for all the console logging
+const DEBUG = true;
+const NETWORKCHECK = true;
+const USE_BURNER_WALLET = false; // toggle burner wallet feature
+const USE_NETWORK_SELECTOR = true;
+
+const web3Modal = Web3ModalSetup();
+
+// 🛰 providers
+const providers = [
+  "https://eth-mainnet.gateway.pokt.network/v1/lb/611156b4a585a20035148406",
+  `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_KEY}`,
+  "https://rpc.scaffoldeth.io:48544",
+];
+
+function App(props) {
+  // specify all the chains your app is available on. Eg: ['localhost', 'mainnet', ...otherNetworks ]
+  // reference './constants.js' for other networks
+  const networkOptions = ["kcc-main","kcc-test"];
+
+  const [injectedProvider, setInjectedProvider] = useState();
+  const [address, setAddress] = useState();
+  const [selectedNetwork, setSelectedNetwork] = useState(networkOptions[0]);
+  const location = useLocation();
+
+  const targetNetwork = NETWORKS[selectedNetwork];
+
+  // 🔭 block explorer URL
+  const blockExplorer = targetNetwork.blockExplorer;
+
+  // load all your providers
+  const localProvider = useStaticJsonRPC([
+    process.env.REACT_APP_PROVIDER ? process.env.REACT_APP_PROVIDER : targetNetwork.rpcUrl,
+  ]);
+  const mainnetProvider = useStaticJsonRPC(providers);
+
+  const logoutOfWeb3Modal = async () => {
+    await web3Modal.clearCachedProvider();
+    if (injectedProvider && injectedProvider.provider && typeof injectedProvider.provider.disconnect == "function") {
+      await injectedProvider.provider.disconnect();
+    }
+    setTimeout(() => {
+      window.location.reload();
+    }, 10);
+  };
+
+  // TODO: use the price from mojitoswap 
+  const price = 0;
+
+  // Use your injected provider from 🦊 Metamask or if you don't have it then instantly generate a 🔥 burner wallet.
+  const userProviderAndSigner = useUserProviderAndSigner(injectedProvider, localProvider, USE_BURNER_WALLET);
+  const userSigner = userProviderAndSigner.signer;
+
+  useEffect(() => {
+    async function getAddress() {
+      if (userSigner) {
+        const newAddress = await userSigner.getAddress();
+        setAddress(newAddress);
+      }
+    }
+    getAddress();
+  }, [userSigner]);
+
+  // You can warn the user if you would like them to be on a specific network
+  const localChainId = localProvider && localProvider._network && localProvider._network.chainId;
+  const selectedChainId =
+    userSigner && userSigner.provider && userSigner.provider._network && userSigner.provider._network.chainId;
+
+
+  const kccContracts = useContractLoader(localProvider,{
+    externalContracts:externalContracts
+  },localChainId);
+
+
+
+  const loadWeb3Modal = useCallback(async () => {
+    const provider = await web3Modal.connect();
+    setInjectedProvider(new ethers.providers.Web3Provider(provider));
+
+    provider.on("chainChanged", chainId => {
+      console.log(`chain changed to ${chainId}! updating providers`);
+      setInjectedProvider(new ethers.providers.Web3Provider(provider));
+    });
+
+    provider.on("accountsChanged", () => {
+      console.log(`account changed!`);
+      setInjectedProvider(new ethers.providers.Web3Provider(provider));
+    });
+
+    // Subscribe to session disconnection
+    provider.on("disconnect", (code, reason) => {
+      console.log(code, reason);
+      logoutOfWeb3Modal();
+    });
+    // eslint-disable-next-line
+  }, [setInjectedProvider]);
+
+  useEffect(() => {
+    if (web3Modal.cachedProvider) {
+      loadWeb3Modal();
+    }
+  }, [loadWeb3Modal]);
+
+
+
+  return (
+    <div className="App">
+      {/* ✏️ Edit the header and change the title to your project name */}
+      <Header
+        link={""}
+        title={"KCC Tools"}
+        subTitle = {"A simple tool for validators. Powered by scalfold-eth"}
+      >
+        <div style={{ position: "relative", display: "flex", flexDirection: "column" }}>
+          <div style={{ display: "flex", flex: 1 }}>
+            {USE_NETWORK_SELECTOR && (
+              <div style={{ marginRight: 20 }}>
+                <NetworkSwitch
+                  networkOptions={networkOptions}
+                  selectedNetwork={selectedNetwork}
+                  setSelectedNetwork={setSelectedNetwork}
+                />
+              </div>
+            )}
+            <Account
+              useBurner={USE_BURNER_WALLET}
+              address={address}
+              localProvider={localProvider}
+              userSigner={userSigner}
+              mainnetProvider={mainnetProvider}
+              price={price}
+              web3Modal={web3Modal}
+              loadWeb3Modal={loadWeb3Modal}
+              logoutOfWeb3Modal={logoutOfWeb3Modal}
+              blockExplorer={blockExplorer}
+            />
+          </div>
+        </div>
+      </Header>
+      <NetworkDisplay
+        NETWORKCHECK={NETWORKCHECK}
+        localChainId={localChainId}
+        selectedChainId={selectedChainId}
+        targetNetwork={targetNetwork}
+        logoutOfWeb3Modal={logoutOfWeb3Modal}
+        USE_NETWORK_SELECTOR={USE_NETWORK_SELECTOR}
+      />
+      <Menu style={{ textAlign: "center", marginTop: 20 }} selectedKeys={[location.pathname]} mode="horizontal">
+        <Menu.Item key="/">
+          <Link to="/">margin</Link>
+        </Menu.Item>
+        <Menu.Item key="/rewards">
+          <Link to="/rewards">rewards</Link>
+        </Menu.Item>
+        <Menu.Item key="/fee">
+          <Link to="/fee">fee</Link>
+        </Menu.Item>
+      </Menu>
+
+      <Switch>
+        <Route exact path="/">
+          <Contract
+            name="margin-management"
+            price={price}
+            signer={userSigner}
+            provider={localProvider}
+            address={address}
+            blockExplorer={blockExplorer}
+            customContract={kccContracts && kccContracts.Validators}
+            show = {["getPoolSelfBallots","depositMargin","redeemMargin","withdrawMargin","isWithdrawable"]}
+          />
+        </Route>
+        <Route exact path="/rewards">
+          <Contract
+            name="rewards"
+            price={price}
+            signer={userSigner}
+            provider={localProvider}
+            address={address}
+            blockExplorer={blockExplorer}
+            customContract={kccContracts && kccContracts.Validators}
+            show = {["claimSelfBallotsReward"]}
+          />
+        </Route>
+        <Route exact path="/fee">
+          <Contract
+            name="fee"
+            price={price}
+            signer={userSigner}
+            provider={localProvider}
+            address={address}
+            blockExplorer={blockExplorer}
+            customContract={kccContracts && kccContracts.Validators}
+            show = {["getPoolfeeShares","setFeeSharesOfValidator","claimFeeReward"]}
+          />
+        </Route>
+      </Switch>
+
+      <ThemeSwitch />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Contract/index.jsx
@@ -0,0 +1,151 @@
+import { LeftCircleFilled } from "@ant-design/icons";
+import { Card } from "antd";
+import { useContractExistsAtAddress, useContractLoader } from "eth-hooks";
+import React, { useMemo, useState } from "react";
+import { useParams } from "react-router-dom";
+import Address from "../Address";
+import Balance from "../Balance";
+import DisplayVariable from "./DisplayVariable";
+import FunctionForm from "./FunctionForm";
+
+
+const noContractDisplay = (
+  <div>
+    Loading...{" "}
+    <div style={{ padding: 32 }}>
+      You need to run{" "}
+      <span
+        className="highlight"
+        style={{ marginLeft: 4, /* backgroundColor: "#f1f1f1", */ padding: 4, borderRadius: 4, fontWeight: "bolder" }}
+      >
+        yarn run chain
+      </span>{" "}
+      and{" "}
+      <span
+        className="highlight"
+        style={{ marginLeft: 4, /* backgroundColor: "#f1f1f1", */ padding: 4, borderRadius: 4, fontWeight: "bolder" }}
+      >
+        yarn run deploy
+      </span>{" "}
+      to see your contract here.
+    </div>
+    <div style={{ padding: 32 }}>
+      <span style={{ marginRight: 4 }} role="img" aria-label="warning">
+        ☢️
+      </span>
+      Warning: You might need to run
+      <span
+        className="highlight"
+        style={{ marginLeft: 4, /* backgroundColor: "#f1f1f1", */ padding: 4, borderRadius: 4, fontWeight: "bolder" }}
+      >
+        yarn run deploy
+      </span>{" "}
+      <i>again</i> after the frontend comes up!
+    </div>
+  </div>
+);
+
+const isQueryable = fn => (fn.stateMutability === "view" || fn.stateMutability === "pure") && fn.inputs.length === 0;
+
+export default function Contract({
+  customContract,
+  account,
+  gasPrice,
+  signer,
+  provider,
+  name,
+  show,
+  price,
+  blockExplorer,
+  chainId,
+  contractConfig,
+}) {
+
+
+  const urlParams = new URLSearchParams(window.location.search);
+  const myParam = urlParams.get('block');
+
+
+  const contract = customContract;
+ 
+
+  const address = contract ? contract.address : "";
+  const contractIsDeployed = useContractExistsAtAddress(provider, address);
+
+  const displayedContractFunctions = useMemo(() => {
+    const results = contract
+      ? Object.entries(contract.interface.functions).filter(
+        fn => fn[1]["type"] === "function" && !(show && show.indexOf(fn[1]["name"]) < 0),
+      )
+      : [];
+    return results;
+  }, [contract, show]);
+
+  const [refreshRequired, triggerRefresh] = useState(false);
+  const contractDisplay = displayedContractFunctions.map(contractFuncInfo => {
+    let contractFunc =
+      contractFuncInfo[1].stateMutability === "view" || contractFuncInfo[1].stateMutability === "pure"
+        ? contract.connect(signer)[contractFuncInfo[0]]
+        : contract.connect(signer)[contractFuncInfo[0]];
+
+    if(contractFuncInfo[1].stateMutability === "view" ){
+      const o = {blockTag: myParam == null ? 'latest' : parseInt(myParam)};
+      const f = contractFunc;
+      contractFunc = async (...args) => { 
+        const res = await f(...args,o); 
+        return res;
+      }
+    }
+
+
+    if (typeof contractFunc === "function") {
+      if (isQueryable(contractFuncInfo[1])) {
+        // If there are no inputs, just display return value
+        return (
+          <DisplayVariable
+            key={contractFuncInfo[1].name}
+            contractFunction={contractFunc}
+            functionInfo={contractFuncInfo[1]}
+            refreshRequired={refreshRequired}
+            triggerRefresh={triggerRefresh}
+            blockExplorer={blockExplorer}
+          />
+        );
+      }
+
+      // If there are inputs, display a form to allow users to provide these
+      return (
+        <FunctionForm
+          key={"FF" + contractFuncInfo[0]}
+          contractFunction={contractFunc}
+          functionInfo={contractFuncInfo[1]}
+          provider={provider}
+          gasPrice={gasPrice}
+          triggerRefresh={triggerRefresh}
+        />
+      );
+    }
+    return null;
+  });
+
+  return (
+    <div style={{ margin: "auto", width: "70vw" }}>
+      <Card
+        title={
+          <div style={{ fontSize: 24 }}>
+            {name}
+            <div style={{ float: "right" }}>
+              <Address value={address} blockExplorer={blockExplorer} />
+              <Balance address={address} provider={provider} price={price} />
+            </div>
+          </div>
+        }
+        size="large"
+        style={{ marginTop: 25, width: "100%" }}
+        loading={contractDisplay && contractDisplay.length <= 0}
+      >
+        {contractIsDeployed ? contractDisplay : noContractDisplay}
+      </Card>
+    </div>
+  );
+}
